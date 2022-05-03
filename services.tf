f13be6a665196d94ca45100475dbccfb0a5e35db
otofune <otofune@otofune.me>
2022-05-04 04:52:36 +0900

    disable services for Cloud Functions (2nd gen)

--- services.tf
+++ services.tf
@@ -3,26 +3,6 @@ resource "google_project_service" "cloudbuild" {
   disable_dependent_services = true
 }
 
-resource "google_project_service" "artifactregistry" {
-  service                    = "artifactregistry.googleapis.com"
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "eventarc" {
-  service                    = "eventarc.googleapis.com"
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "run" {
-  service                    = "run.googleapis.com"
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "logging" {
-  service                    = "logging.googleapis.com"
-  disable_dependent_services = true
-}
-
 resource "google_project_service" "pubsub" {
   service                    = "pubsub.googleapis.com"
   disable_dependent_services = true
