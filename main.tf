7a50f1efe7f1be6660cfe4c348f301ae666ffb34
takanakahiko <takanakahiko@gmail.com>
2022-05-05 23:17:50 +0900

    Revert "upgrade instance type"
    This reverts commit f827eea66e78de94b40005e4404699f97c071057.

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ provider "google" {
 
 resource "google_compute_instance" "minecraft" {
   name         = "minecraft-instance"
-  machine_type = "n1-standard-2"
+  machine_type = "n1-standard-1"
   zone         = local.zone
   tags         = ["minecraft"]
-  metadata_startup_script = "docker run -d --rm -p 42865:25565 -e EULA=TRUE -e VERSION=1.18.2 -e MEMORY=4G -e OPS=rinsuki,takanakahiko -v /var/minecraft:/data itzg/minecraft-server:latest;"
+  metadata_startup_script = "docker run -d --rm -p 42865:25565 -e EULA=TRUE -e VERSION=1.18.2 -e MEMORY=2G -e OPS=rinsuki,takanakahiko -v /var/minecraft:/data itzg/minecraft-server:latest;"
   metadata = {
     enable-oslogin = "TRUE"
   }
