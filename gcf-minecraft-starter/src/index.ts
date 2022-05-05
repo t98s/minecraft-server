34d59d82c6bfae0f6a31804223289db9e731ec15
otofune <otofune@otofune.me>
2022-05-06 00:41:35 +0900

    Add prettier

--- gcf-minecraft-starter/src/index.ts
+++ gcf-minecraft-starter/src/index.ts
@@ -5,7 +5,11 @@ import functions from '@google-cloud/functions-framework'
 import pubsub from '@google-cloud/pubsub'
 import axios from 'axios'
 import { Routes } from 'discord-api-types/v9'
-import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions'
+import {
+    InteractionResponseType,
+    InteractionType,
+    verifyKey,
+} from 'discord-interactions'
 import { parseEnv } from './env'
 import { createLogger } from './logger'
 
@@ -13,20 +17,19 @@ const env = parseEnv(process.env)
 const rest = new REST({
     version: '9',
 }).setToken(env.DISCORD_APIKEY)
-const topic = (new pubsub.PubSub()).topic(env.GCF_INVOKER_TOPIC)
+const topic = new pubsub.PubSub().topic(env.GCF_INVOKER_TOPIC)
 
 const logger = createLogger()
 
-async function init () {
+async function init() {
     logger.info('start refreshing slash commands')
-    await rest.put(
-        Routes.applicationCommands(env.DISCORD_APPLICATION_ID),
-        {
-            body: [
-                new SlashCommandBuilder().setName('startmc').setDescription('Start minecraft VM instance'),
-            ],
-        },
-    )
+    await rest.put(Routes.applicationCommands(env.DISCORD_APPLICATION_ID), {
+        body: [
+            new SlashCommandBuilder()
+                .setName('startmc')
+                .setDescription('Start minecraft VM instance'),
+        ],
+    })
 }
 
 init().catch(e => {
@@ -51,51 +54,53 @@ functions.http('interaction', async (req, res) => {
             return res.status(401).end('invalid signature')
         }
 
-        if (!verifyKey(
-            req.rawBody,
-            signature,
-            signatureTimestamp,
-            env.DISCORD_PUBLIC_KEY,
-        )) {
+        if (
+            !verifyKey(
+                req.rawBody,
+                signature,
+                signatureTimestamp,
+                env.DISCORD_PUBLIC_KEY
+            )
+        ) {
             return res.status(401).end('invalid request signature')
         }
         logger.info('verify successful')
         logger.info(JSON.stringify(req.body, null, 4))
 
         switch (req.body.type) {
-        case InteractionType.PING:
-            logger.info('ping')
+            case InteractionType.PING:
+                logger.info('ping')
 
-            return res.send({
-                type: InteractionResponseType.PONG,
-            })
-        case InteractionType.APPLICATION_COMMAND:
-            switch (req.body.data.name) {
-            case 'startmc': {
-                logger.info('startmc')
-
-                // Cloud Functions はレスポンスを返した時点でそのあとの処理が実行される保証がなくなる
-                // そのため別の関数に移譲する
-                await topic.publishMessage({
-                    json: {
-                        webhookUrl: `https://discord.com/api/webhooks/${env.DISCORD_APPLICATION_ID}/${req.body.token}`,
-                    },
+                return res.send({
+                    type: InteractionResponseType.PONG,
                 })
-                res.send({
-                    type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
-                })
+            case InteractionType.APPLICATION_COMMAND:
+                switch (req.body.data.name) {
+                    case 'startmc': {
+                        logger.info('startmc')
 
-                return
-            }
+                        // Cloud Functions はレスポンスを返した時点でそのあとの処理が実行される保証がなくなる
+                        // そのため別の関数に移譲する
+                        await topic.publishMessage({
+                            json: {
+                                webhookUrl: `https://discord.com/api/webhooks/${env.DISCORD_APPLICATION_ID}/${req.body.token}`,
+                            },
+                        })
+                        res.send({
+                            type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+                        })
+
+                        return
+                    }
+                    default:
+                        logger.info('unknown command received')
+
+                        return res.status(500).end('unknown command received')
+                }
             default:
-                logger.info('unknown command received')
+                logger.info('unknown request received')
 
-                return res.status(500).end('unknown command received')
-            }
-        default:
-            logger.info('unknown request received')
-
-            return res.status(500).end('unknown request received')
+                return res.status(500).end('unknown request received')
         }
     } catch (e) {
         logger.error(e)
@@ -105,9 +110,12 @@ functions.http('interaction', async (req, res) => {
     }
 })
 
-type StartInstanceInit = Omit<pubsub.protos.google.pubsub.v1.PubsubMessage, 'publishTime' | 'messageId'> & { data: string }
+type StartInstanceInit = Omit<
+    pubsub.protos.google.pubsub.v1.PubsubMessage,
+    'publishTime' | 'messageId'
+> & { data: string }
 
-export async function startInstance ({ data }: StartInstanceInit) {
+export async function startInstance({ data }: StartInstanceInit) {
     const { webhookUrl } = JSON.parse(Buffer.from(data, 'base64').toString())
     const sendFollowUpMessage = (msg: string) => {
         logger.info(`sending follow up message: ${msg}`)
@@ -132,7 +140,9 @@ export async function startInstance ({ data }: StartInstanceInit) {
         const [current] = await instancesClient.get(targetInstance)
 
         if (current.status !== 'TERMINATED') {
-            await sendFollowUpMessage(`終了状態ではないため、起動を行いません (${current.status})`)
+            await sendFollowUpMessage(
+                `終了状態ではないため、起動を行いません (${current.status})`
+            )
 
             return
         }
@@ -149,9 +159,13 @@ export async function startInstance ({ data }: StartInstanceInit) {
 
         if (waitedOperation.status === 'DONE') {
             if (waitedOperation.error?.errors) {
-                const messages = waitedOperation.error.errors.map(e => e.message || e.code || 'unknown').join(', ')
+                const messages = waitedOperation.error.errors
+                    .map(e => e.message || e.code || 'unknown')
+                    .join(', ')
 
-                await sendFollowUpMessage(`起動しようとしましたが、エラーが発生しました (${messages})`)
+                await sendFollowUpMessage(
+                    `起動しようとしましたが、エラーが発生しました (${messages})`
+                )
             } else {
                 await sendFollowUpMessage('起動しました')
             }
--- gcf-minecraft-starter/src/logger.ts
+++ gcf-minecraft-starter/src/logger.ts
@@ -4,27 +4,32 @@ import util from 'util'
 export type LogSeverity = 'DEBUG' | 'ERROR' | 'INFO'
 
 export interface Logger {
-  log (serverity: LogSeverity, message: string | unknown): void;
-  info (message: string | unknown): void;
-  error (message: string | unknown): void;
-  debug (message: string | unknown): void;
+    log(serverity: LogSeverity, message: string | unknown): void
+    info(message: string | unknown): void
+    error(message: string | unknown): void
+    debug(message: string | unknown): void
 }
 
-export function createLogger (): Logger {
+export function createLogger(): Logger {
     const logger: Logger = {
-        log (serverity, message) {
-            console.log(JSON.stringify({
-                serverity,
-                message: typeof message === 'string' ? message : util.inspect(message),
-            }))
+        log(serverity, message) {
+            console.log(
+                JSON.stringify({
+                    serverity,
+                    message:
+                        typeof message === 'string'
+                            ? message
+                            : util.inspect(message),
+                })
+            )
         },
-        error (message) {
+        error(message) {
             logger.log('ERROR', message)
         },
-        info (message) {
+        info(message) {
             logger.log('INFO', message)
         },
-        debug (message) {
+        debug(message) {
             logger.log('DEBUG', message)
         },
     }
